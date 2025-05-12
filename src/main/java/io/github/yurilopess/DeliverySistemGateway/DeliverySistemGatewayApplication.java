5cfa57da7864a21756f79ffcde3dc9bc30b1e954
YuriLopess <costalopesyuri@gmail.com>
2025-05-12 19:45:50 -0300

    initial commit

---
+++ src/main/java/io/github/yurilopess/DeliverySistemGateway/DeliverySistemGatewayApplication.java
@@ -0,0 +1,13 @@
+package io.github.yurilopess.DeliverySistemGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverySistemGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverySistemGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/yurilopess/DeliverySistemGateway/DeliverySistemGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yurilopess.DeliverySistemGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverySistemGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
