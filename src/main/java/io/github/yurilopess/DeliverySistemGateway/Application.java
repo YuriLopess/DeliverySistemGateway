2f548827dd319325cc641306262da0317fe116cf
YuriLopess <costalopesyuri@gmail.com>
2025-05-13 20:32:10 -0300

    feat: configure api gateway

--- src/main/java/io/github/yurilopess/DeliverySistemGateway/Application.java
+++ src/main/java/io/github/yurilopess/DeliverySistemGateway/Application.java
@@ -2,12 +2,14 @@ package io.github.yurilopess.DeliverySistemGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-public class DeliverySistemGatewayApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DeliverySistemGatewayApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
